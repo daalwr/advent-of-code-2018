6d15ca35aec82f7653ded1aaf0816dbe61f4e0b3
Daniel Wright <daniel.wright2@sky.uk>
2018-12-14 21:23:16 +0000

    Day 12

---
+++ solutions/day12/day12_1.ts
@@ -0,0 +1,70 @@
+import fs from "fs";
+
+// tslint:disable-next-line:max-line-length
+const initialState = "##..#.#.#..##..#..##..##..#.#....#.....##.#########...#.#..#..#....#.###.###....#..........###.#.#..";
+const fileText = fs.readFileSync("day12_input.txt").toString();
+const rules = fileText.split("\n").map((row) => {
+  const pair = row.split(" => ");
+  return {
+    pattern: pair[0],
+    result: pair[1],
+  };
+});
+
+console.log(rules);
+
+const padding = 30;
+
+const paddedInitialState = ".".repeat(padding) + initialState + ".".repeat(padding);
+console.log(paddedInitialState);
+
+function calculateNextGeneration(input: string) {
+  let res = "";
+  for (let i = 0; i < input.length; i++) {
+    const ll = getValue(input, i - 2);
+    const l = getValue(input, i - 1);
+    const c = getValue(input, i);
+    const r = getValue(input, i + 1);
+    const rr = getValue(input, i + 2);
+
+    const previousPattern = ll + l + c + r + rr;
+
+    res += calculateResult(previousPattern);
+  }
+  return res;
+}
+
+function getValue(input: string, index: number) {
+  if (index < 0 || index >= input.length) {
+    return ".";
+  } else {
+    return input.slice(index, index + 1);
+  }
+}
+
+function calculateResult(pattern: string): string {
+  const rule = rules.find((x) => x.pattern === pattern);
+  if (rule !== undefined) {
+    return rule.result;
+  } else {
+    return ".";
+  }
+}
+
+let currentGeneration = paddedInitialState;
+console.log(`0: ${currentGeneration}`);
+for (let g = 1; g <= 20; g++) {
+  currentGeneration = calculateNextGeneration(currentGeneration);
+  console.log(`${g}: ${currentGeneration}`);
+}
+
+const result = currentGeneration;
+
+let score = 0;
+for (let i = 0; i < currentGeneration.length; i++) {
+  const potScore = i - padding;
+  if (currentGeneration.charAt(i) === "#") {
+    score += potScore;
+  }
+}
+console.log("Final result: " + score);
